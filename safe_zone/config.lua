1655e739829381ba7ca7360bd38e77ab329469d1
Anxxity <90343571+Anxxity@users.noreply.github.com>
2023-01-14 00:51:11 +0530

    Update config.lua

--- safe_zone/config.lua
+++ safe_zone/config.lua
@@ -1,7 +1,7 @@
 Config={}
 
 Config.zones=
-{coords= vector3(755.02, -298.71, 59.89) , color= ,transparent =vector3(755.02, -298.71, 59.89),radius }
+{coords= vector3(755.02, -298.71, 59.89) , color= ,transparent =vector3(755.02, -298.71, 59.89),radius = 5.00 }
 
 
 }
